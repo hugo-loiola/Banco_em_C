f06fe08e6f15d6b772cba49d323615560ba6dfe9
Hugo Loiola <hugoloiola.1@gmail.com>
2023-11-23 18:15:30 -0300

    testes

--- c.c
+++
@@ -1,1 +0,0 @@
-bla vla bla 
--- deposito.c
+++ deposito.c
@@ -4,18 +4,20 @@
 
 #define SENHA_PADRAO "money"
 void limparTela();
-int main() {
-char BancoNome[] = "O_BANCO";
+int main()
+{
+    char BancoNome[] = "O_BANCO";
     char senhaDigitada[20];
     int opcao;
-    
 
-    do {
+    do
+    {
         limparTela();
         printf("Digite a senha: ");
         scanf("%s", senhaDigitada);
 
-        if (strcmp(senhaDigitada, SENHA_PADRAO) != 0) {
+        if (strcmp(senhaDigitada, SENHA_PADRAO) != 0)
+        {
             printf("Senha incorreta! Tente novamente.\n");
             printf("Pressione Enter para continuar...");
             getchar();
@@ -23,11 +25,12 @@ char BancoNome[] = "O_BANCO";
         }
     } while (strcmp(senhaDigitada, SENHA_PADRAO) != 0);
 
-    while (1) {
+    while (1)
+    {
         limparTela();
 
         printf("%s\n", BancoNome);
-        printf("\n------------ MENU ------------\n");8
+        printf("\n------------ MENU ------------\n");
         printf("1 - Criar nova Conta\n");
         printf("2 - Depositar e sacar dinheiro em uma nova conta\n");
         printf("3 - Mostrar saldo total de cada conta\n");
@@ -39,44 +42,43 @@ char BancoNome[] = "O_BANCO";
         printf("\nEscolha uma opcao: ");
         scanf("%d", &opcao);
 
-        switch (opcao) {
-            case 1:
-                printf("1 - COLOQUE SUA FUNCAO\n");
-                break;
-            case 2:
-                printf("2 - COLOQUE SUA FUNCAO\n");
-                break;
-            case 3:
-                printf("3 - COLOQUE SUA FUNCAO\n");
-                break;
-             case 4:
-                printf("4 - COLOQUE SUA FUNCAO\n");
-                break;
-             case 5:
-                printf("5 - COLOQUE SUA FUNCAO\n");
-                break;
-            case 6:
-                printf("6 - COLOQUE SUA FUNCAO\n");
-                break;
-            case 7:
-                printf("7 - COLOQUE SUA FUNCAO\n");
-                break;
-            case 8:
-                printf("Fim da operacao %s\n", BancoNome);
-                exit(0);
-            default:
-                printf("Opcao incorreta. Pressione Enter para continuar...");
-                getchar();  
-                getchar();  
-                break;
+        switch (opcao)
+        {
+        case 1:
+            printf("1 - COLOQUE SUA FUNCAO\n");
+            break;
+        case 2:
+            printf("2 - COLOQUE SUA FUNCAO\n");
+            break;
+        case 3:
+            printf("3 - COLOQUE SUA FUNCAO\n");
+            break;
+        case 4:
+            printf("4 - COLOQUE SUA FUNCAO\n");
+            break;
+        case 5:
+            printf("5 - COLOQUE SUA FUNCAO\n");
+            break;
+        case 6:
+            printf("6 - COLOQUE SUA FUNCAO\n");
+            break;
+        case 7:
+            printf("7 - COLOQUE SUA FUNCAO\n");
+            break;
+        case 8:
+            printf("Fim da operacao %s\n", BancoNome);
+            exit(0);
+        default:
+            printf("Opcao incorreta. Pressione Enter para continuar...");
+            getchar();
+            getchar();
+            break;
         }
     }
 
-
-
     return 0;
-
 }
-void limparTela() {
+void limparTela()
+{
     system("cls");
 }
